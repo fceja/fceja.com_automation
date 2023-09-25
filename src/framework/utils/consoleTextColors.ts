ff092be6120bf127aeb1ca6f5dce46dce4ad1802
jenkins-bot-fceja <zen-fleury.0i@icloud.com>
2023-09-25 10:42:07 -0700

    add console text colors util

---
+++ src/framework/utils/consoleTextColors.ts
@@ -0,0 +1,7 @@
+export const RED = "\x1b[31m";
+export const GREEN = "\x1b[32m";
+export const YELLOW = "\x1b[33m";
+export const BLUE = "\x1b[34m";
+export const MAGENTA = "\x1b[35m";
+export const CYAN = "\x1b[36m";
+export const RESET = "\x1b[0m";
