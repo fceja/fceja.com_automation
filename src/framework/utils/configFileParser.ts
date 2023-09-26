f63a58cd794ba7d15c13c74c4276a648a3b9c4fd
jenkins-bot-fceja <zen-fleury.0i@icloud.com>
2023-09-25 17:13:20 -0700

    mini refactor configFileParse, simplify

--- src/framework/utils/configFileParser.ts
+++ src/framework/utils/configFileParser.ts
@@ -22,33 +22,21 @@ export default class ConfigFileParser {
   }
 
   startUp() {
+    this.getConfigFilePathArg();
     this.validateConfigFilePathArg();
     this.parseConfigFile();
   }
 
-  parseConfigFile() {
-    /*
-     * - Parse and stores config file json props in instance
-     */
-    const configDataJson = require(this.CONFIG_FILE_PATH);
-
-    this.CONFIG_JSON_DATA = {
-      ...this.CONFIG_JSON_DATA,
-      ...configDataJson,
-    };
-  }
-
-  validateConfigFilePathArg() {
+  getConfigFilePathArg() {
     /*
      * - Parses 'CONFIG_FILE_PATH_ARG' from the command line arguments,
      *   and stores it in the 'CONFIG_FILE_PATH' property.
      * - Throws an error if not provided.
      *
-     * - Example CLI usage:
+     * - Example cli usage:
      *   - tsc && node {path_to_test_file} CONFIG_FILE_PATH_ARG={path_to_config_file}
      *   - tsc && node dist/frontend/tests/firstTest.js CONFIG_FILE_PATH_ARG=configs/localConfig.json
      */
-
     // parse cli arguments
     const configFilePathArg = process.argv.find((arg) =>
       arg.startsWith("CONFIG_FILE_PATH_ARG")
@@ -66,13 +54,29 @@ export default class ConfigFileParser {
 
     // store config file path
     this.CONFIG_FILE_PATH = configFilePathArg.split("=")[1];
+  }
 
-    // validate file exists
+  parseConfigFile() {
+    /*
+     * - Parse and stores config file json props in instance
+     */
+    const configDataJson = require(this.CONFIG_FILE_PATH);
+
+    this.CONFIG_JSON_DATA = {
+      ...this.CONFIG_JSON_DATA,
+      ...configDataJson,
+    };
+  }
+
+  validateConfigFilePathArg() {
+    /*
+     * - Validates 'CONFIG_FILE_PATH_ARG' file path exists
+     */
     if (!fs.existsSync(this.CONFIG_FILE_PATH)) {
       throw Error(
         `${addColorCodeToString(
           "red",
-          "Provided config file path does not exist"
+          "Provided 'CONFIG_FILE_PATH_ARG' file path does not exist"
         )}` +
           `\n${addColorCodeToString("magenta", "CONFIG_FILE_PATH_ARG")} -> '${
             this.CONFIG_FILE_PATH
