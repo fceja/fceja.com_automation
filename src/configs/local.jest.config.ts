82f1b5edb350fea98da5c8fd3afd7486ad063a27
fceja <cisco_ceja@icloud.com>
2023-10-02 15:03:22 -0700

    add local config files

--- src/configs/local.jest.config.ts
+++ src/configs/local.jest.config.ts
@@ -1,9 +1,10 @@
 export default {
-  roots: ["<rootDir>/../"],
+  roots: ["<rootDir>/../.."],
   preset: "ts-jest",
   testEnvironment: "node",
   testMatch: ["**/*.test.ts"],
   testTimeout: 15000,
   moduleFileExtensions: ["ts", "js"],
+  setupFilesAfterEnv: ["./local.jest.setup.ts"],
   verbose: true,
 };
---
+++ src/configs/local.jest.setup.ts
@@ -0,0 +1,2 @@
+process.env.CONFIG_FILE_PATH_ARG =
+  "/Users/fceja/projects/fceja-org/fceja_automation/src/configs/local.config.json";
