4b9d39a4fd61c0973eb83d2faa413688edf55ace
fceja <cisco_ceja@icloud.com>
2023-10-13 23:34:33 -0700

    remove, no longer needed

--- src/frontend/fceja/tests/CardData.ts
+++ src/frontend/fceja/tests/CardData.ts
@@ -12,11 +12,5 @@ export const CardCarouselCardData: ReadonlyArray<CardDataItem> = Object.freeze([
     description:
       "Imatation of card game, where game is won by selecting a predetermined pattern.",
     urlLink: "https://github.com/fceja/bingo_game",
-  },
-  {
-    imagePath: "assets/orange.png",
-    title: "Test - Orange",
-    description: "Test - Orange Description.",
-    urlLink: "http://localhost:3000",
-  },
+  }
 ]);
